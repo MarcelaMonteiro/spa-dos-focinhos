52bb908cac37b0be00bec285e1bffde0068018f4
Marcela <marcelapmont123@gmail.com>
2026-02-05 16:51:53 -0300

    fix cors for vercel

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://spa-dos-focinhos.vercel.app'],
     credentials: true,
   });
   app.useGlobalPipes(
